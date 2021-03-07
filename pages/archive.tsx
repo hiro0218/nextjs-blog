e005a3932f7ee72a9479b2d3a2a610de22797139
hiro <hiro0218@gmail.com>
2021-03-07 17:34:40 +0900

    test commit

--- pages/archive.tsx
+++ pages/archive.tsx
@@ -1,11 +1,32 @@
+import { GetStaticProps } from "next";
+import fs from "fs-extra";
+import path from "path";
 import Layout from "../components/layout";
 
-const Archive = () => {
+const Archive = ({ archives }) => {
   return (
     <Layout>
       <p>Archive page</p>
+      <ul>
+        {archives.map((archive) => (
+          <li>
+            <p>{archive.title}</p>
+          </li>
+        ))}
+      </ul>
     </Layout>
   );
 };
 
 export default Archive;
+
+export const getStaticProps: GetStaticProps = async () => {
+  const dataPath = path.join(process.cwd(), "data/posts.json");
+  const posts = fs.readJsonSync(dataPath);
+
+  return {
+    props: {
+      archives: posts,
+    },
+  };
+};
